fe8b95ef24980b7597ad82cdf5cbf87ba8dcf9ad
achiraissa <achira.issa@bumail.net>
2024-03-13 09:31:31 +0700

    Resource manager

---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResourceManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] woodTreePrefab;
+
+    [SerializeField]
+    private Transform woodTreeParent;
+
+    [SerializeField]
+    private ResourceSource[] resources;
+
+    public static ResourceManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    private void FindAllResource()
+    {
+        resources = FindObjectsOfType<ResourceSource>();
+    }
+
+
+    void Start()
+    {
+        FindAllResource();
+    }
+
+
+}
