3cdda9a1b392b0e3dc0b102f02ad83b129f34bf2
achiraissa <achira.issa@bumail.net>
2024-03-20 09:41:02 +0700

    Bug Fix

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -17,6 +17,11 @@ public class UnitSelect : MonoBehaviour
     public List<Unit> CurUnits { get { return curUnits; } }
 
     [SerializeField]
+    private RectTransform selectionBox;
+    private Vector2 oldAnchoredPos;//Box old anchored position
+    private Vector2 startPos;//point where mouse is down
+
+    [SerializeField]
     private ResourceSource curResource; //current selected resource
 
     private Camera cam;
@@ -130,10 +135,56 @@ public class UnitSelect : MonoBehaviour
         InfoManager.instance.ClearAllInfo();
         ActionManager.instance.ClearAllInfo();
     }
+
+    private void UpdateSelectionBox(Vector3 mousePos)
+    {
+        //Debug.Log("Mouse Pos - " + curMousePos);
+        if (!selectionBox.gameObject.activeInHierarchy && curBuilding == null)
+            selectionBox.gameObject.SetActive(true);
+
+        float width = mousePos.x - startPos.x;
+        float height = mousePos.y - startPos.y;
+
+        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        selectionBox.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
+
+        //store old position for real unit selection
+        oldAnchoredPos = selectionBox.anchoredPosition;
+    }
+
+    private void ReleaseSelectionBox(Vector2 mousePos)
+    {
+        //Debug.Log("Step 2 - " + _doubleClickMode);
+        Vector2 min; //down-left corner
+        Vector2 max; //top-right corner
+
+        selectionBox.gameObject.SetActive(false);
+
+        min = oldAnchoredPos - (selectionBox.sizeDelta / 2);
+        max = oldAnchoredPos + (selectionBox.sizeDelta / 2);
+
+        //Debug.Log("min = " + min);
+        //Debug.Log("max = " + max);
+
+        foreach (Unit unit in GameManager.instance.MyFaction.AliveUnits)
+        {
+            Vector2 unitPos = cam.WorldToScreenPoint(unit.transform.position);
+
+            if (unitPos.x > min.x && unitPos.x < max.x && unitPos.y > min.y && unitPos.y < max.y)
+            {
+                curUnits.Add(unit);
+                unit.ToggleSelectionVisual(true);
+            }
+        }
+        selectionBox.sizeDelta = new Vector2(0, 0); //clear Selection Box's size;
+    }
+
     void Start()
     {
         cam = Camera.main;
         layerMask = LayerMask.GetMask("Unit", "Building", "Resource", "Ground");
+
+        selectionBox = MainUI.instance.SelectionBox;
 
         instance = this;
     }
@@ -142,6 +193,8 @@ public class UnitSelect : MonoBehaviour
         //mouse down
         if (Input.GetMouseButtonDown(0))
         {
+            startPos = Input.mousePosition;
+
             //if click Ui, don't clear
             if (EventSystem.current.IsPointerOverGameObject())
             {
@@ -150,9 +203,17 @@ public class UnitSelect : MonoBehaviour
             ClearEverything();
         }
 
+        // mouse held down
+        if (Input.GetMouseButton(0))
+        {
+            //Debug.Log("Mouse held down");
+            UpdateSelectionBox(Input.mousePosition);
+        }
+
         // mouse up
         if (Input.GetMouseButtonUp(0))
         {
+            ReleaseSelectionBox(Input.mousePosition);
             TrySelect(Input.mousePosition);
         }
     }
