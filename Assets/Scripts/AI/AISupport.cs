18b14d3a96283ed1878801940ccecff8f6e8b790
achiraissa <achira.issa@bumail.net>
2024-04-03 09:59:25 +0700

    Prepare For Create HQ

--- Assets/Scripts/AI/AISupport.cs
+++ Assets/Scripts/AI/AISupport.cs
@@ -13,6 +13,15 @@ public class AISupport : MonoBehaviour
     [SerializeField] private List<GameObject> workers = new List<GameObject>(); //worker
     public List<GameObject> Workers { get { return workers; } }
 
+    [SerializeField] private List<GameObject> hq = new List<GameObject>();
+    public List<GameObject> HQ { get { return hq; } }
+
+    [SerializeField] private List<GameObject> houses = new List<GameObject>();
+    public List<GameObject> Houses { get { return houses; } }
+
+    [SerializeField] private List<GameObject> barracks = new List<GameObject>();
+    public List<GameObject> Barracks { get { return barracks; } }
+
     [SerializeField] private Faction faction;
     public Faction Faction { get { return faction; } }
 
@@ -24,6 +33,9 @@ public class AISupport : MonoBehaviour
 
         foreach (Unit u in faction.AliveUnits)
         {
+            if (u.gameObject == null)
+                continue;
+
             if (u.IsBuilder) //if it is a builder
                 builders.Add(u.gameObject);
 
@@ -33,9 +45,26 @@ public class AISupport : MonoBehaviour
             if (!u.IsBuilder && !u.IsWorker) //if it is a fighter
                 fighters.Add(u.gameObject);
         }
+
+        hq.Clear();
+        houses.Clear();
+        barracks.Clear();
+
+        foreach(Building b in faction.AliveBuildings)
+        {
+            if(b == null)
+                continue;
+            if(b.IsHQ)
+                hq.Add(b.gameObject);
+            if (b.IsHousing)
+                houses.Add(b.gameObject);
+            if (b.IsBarrack)
+                barracks.Add(b.gameObject);
+
+        }
     }
 
-    void Start()
+    void Awake()
     {
         faction = GetComponent<Faction>();
     }
